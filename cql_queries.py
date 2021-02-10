ef18c8299e2dc9a176b4bfc0edd3a22312c5f42f
Vinícius <viniciusarre@gmail.com>
2021-02-10 09:33:02 -0300

    Improve CQL data types, add error-handling to create_tables and add insertion and querying to cql_queries

--- cql_queries.py
+++ cql_queries.py
@@ -1,13 +1,20 @@
-song_artist_table_drop = 'DROP TABLE IF EXISTS song_artist;' # CQL query for dropping the song_artist table
-user_song_artist_table_drop = 'DROP TABLE IF EXISTS user_song_artist;' # CQL query for dropping the user_song_artist table
-user_song_table_drop = 'DROP TABLE IF EXISTS user_song;' # CQL query for dropping the song_artist table
+
+
+############################### CREATE  / DROP FLOW #############################
+
+# CQL query for dropping the song_artist table
+song_artist_table_drop = 'DROP TABLE IF EXISTS song_artist;'
+# CQL query for dropping the user_song_artist table
+user_song_artist_table_drop = 'DROP TABLE IF EXISTS user_song_artist;'
+# CQL query for dropping the song_artist table
+user_song_table_drop = 'DROP TABLE IF EXISTS user_song;'
 
 # CQL query for creating the song_artist table
 song_artist_table_create = (
     'CREATE TABLE IF NOT EXISTS song_artist (\
        artist TEXT,  \
        song TEXT, \
-       song_length NUMERIC, \
+       song_length DECIMAL, \
        session_id INT, \
        item_in_session INT, \
        PRIMARY KEY (session_id, item_in_session)\
@@ -19,7 +26,7 @@ user_song_artist_table_create = (
     'CREATE TABLE IF NOT EXISTS user_song_artist(\
        artist TEXT,  \
        song TEXT, \
-       song_length NUMERIC, \
+       song_length DECIMAL, \
        user_id INT, \
        first_name TEXT, \
        last_name TEXT, \
@@ -38,8 +45,47 @@ user_song_table_create = (
         PRIMARY KEY (song)\
     )'
 )
-# create table queries tuple
-create_table_queries = (song_artist_table_create, user_song_artist_table_create, user_song_table_create)
 
 # create table queries tuple
-drop_table_queries = (song_artist_table_drop, user_song_artist_table_drop, user_song_table_drop)
+create_table_queries = (song_artist_table_create,
+                        user_song_artist_table_create, user_song_table_create)
+
+# create table queries tuple
+drop_table_queries = (song_artist_table_drop,
+                      user_song_artist_table_drop, user_song_table_drop)
+
+
+############################### END OF CREATE  / DROP FLOW #############################
+
+############################### INSERTIONS ###############################
+
+song_artist_insert = "INSERT INTO song_artist \
+    (artist, song, song_length, session_id, item_in_session) \
+    VALUES (%s, %s, %s, %s, %s)\
+"
+
+user_song_artist_insert = "INSERT INTO user_song_artist \
+    (artist, song, song_length, user_id, first_name, last_name, session_id, item_in_session) \
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)\
+"
+
+user_song_insert = "INSERT INTO user_song \
+    (song, first_name, last_name) \
+    VALUES (%s, %s, %s)\
+"
+###############################################################################
+
+############################### QUERIES ###############################
+
+song_artist_query = "\
+  SELECT artist, song, song_length FROM song_artist WHERE session_id = %s AND item_in_session = %s \
+"
+
+user_song_artist_query = "\
+  SELECT artist, song, first_name, last_name FROM user_song_artist WHERE user_id = %s AND session_id = %s \
+"
+
+user_song_query = "\
+    SELECT first_name, last_name FROM user_song WHERE song = %s \
+"
+###############################################################################
--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
 from cassandra.cluster import Cluster
-from sql_queries import create_table_queries, drop_table_queries
+from cql_queries import create_table_queries, drop_table_queries
 
 
 def create_or_connect_to_keyspace():
@@ -9,18 +9,20 @@ def create_or_connect_to_keyspace():
 
     """
     try:
-        cluster = Cluster(['127.0.0.1']) #If you have a locally installed Apache Cassandra instance
+        # If you have a locally installed Apache Cassandra instance
+        cluster = Cluster(['127.0.0.1'])
         session = cluster.connect()
         session.execute("""
             CREATE KEYSPACE IF NOT EXISTS sparkify 
             WITH REPLICATION = 
             { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"""
-        )
+                        )
         session.set_keyspace('sparkify')
 
     except Exception as e:
         print(e)
-    
+        raise Exception(e)
+
     return cluster, session
 
 
@@ -28,26 +30,29 @@ def drop_tables(session):
     """
 
     Drops each table using the queries in `drop_table_queries` list.
-    
+
     """
     for query in drop_table_queries:
         try:
             session.execute(query)
         except Exception as e:
             print(e)
+            raise Exception(e)
+
 
 def create_tables(session):
     """
 
     Creates each table using the queries in `create_table_queries` list. 
-    
+
     """
     for query in create_table_queries:
         try:
             session.execute(query)
         except Exception as e:
             print(e)
-    
+            raise Exception(e)
+
 
 def main():
     """
@@ -56,15 +61,20 @@ def main():
     - Drops all tables
     - Creates all tables
     - Closes the session and cluster
-    
+
     """
+    try:
+        cluster, session = create_or_connect_to_keyspace()
+        drop_tables(session)
+        create_tables(session)
 
-    cluster, session = create_or_connect_to_keyspace()
-    drop_tables(session)
-    create_tables(session)
+        session.shutdown()
+        cluster.shutdown()
+        print("Tables created successfully!")
 
-    session.shutdown()
-    cluster.shutdown()
+    except Exception as e:
+        print('An error occurred ', e)
 
-    
 
+if __name__ == '__main__':
+    main()
